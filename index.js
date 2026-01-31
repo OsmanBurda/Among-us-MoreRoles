0b993285e3a77503c101ec3f12d18b891d87c7d8
OsmanBurda <osmanceviker0@gmail.com>
2026-01-31 22:53:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http").createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(http);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+app.use(express.static(__dirname + '/'));
 
-const PORT = process.env.PORT || 3000;
-
-// root'tan dosya ver
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+let players = {};
+io.on('connection', (socket) => {
+    // Doğuş yeri: Kafeterya orta boşluk
+    players[socket.id] = { id: socket.id, x: 750, y: 300, color: 'red', name: "Osman" };
+    io.emit('currentPlayers', players);
+    socket.on('playerMovement', (data) => {
+        if (players[socket.id]) { 
+            players[socket.id].x = data.x; 
+            players[socket.id].y = data.y;
+            socket.broadcast.emit('playerMoved', players[socket.id]); 
+        }
+    });
+    socket.on('disconnect', () => { delete players[socket.id]; io.emit('playerDisconnected', socket.id); });
 });
-
-app.get("/client.js", (req, res) => {
-  res.sendFile(__dirname + "/client.js");
-});
-
-// oyuncular
-const players = {};
-
-io.on("connection", socket => {
-  players[socket.id] = {
-    x: 60,
-    y: 60,
-    color: `hsl(${Math.random() * 360},100%,50%)`
-  };
-
-  socket.emit("init", { id: socket.id, players });
-  socket.broadcast.emit("join", { id: socket.id, player: players[socket.id] });
-
-  socket.on("move", data => {
-    if (!players[socket.id]) return;
-    players[socket.id].x = data.x;
-    players[socket.id].y = data.y;
-    socket.broadcast.emit("move", { id: socket.id, x: data.x, y: data.y });
-  });
-
-  socket.on("disconnect", () => {
-    delete players[socket.id];
-    io.emit("leave", socket.id);
-  });
-});
-
-http.listen(PORT, () => console.log("Server up:", PORT));
+http.listen(process.env.PORT || 3000);
