5c254dc5598504670fd9bef22fc0544367ad4b36
OsmanBurda <osmanceviker0@gmail.com>
2026-01-31 23:25:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,37 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+
 app.use(express.static(__dirname + '/'));
 
 let players = {};
 const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'cyan'];
 
 io.on('connection', (socket) => {
+    // Kafeterya'nın ortasında, masaların arasındaki boşlukta doğarsın
     players[socket.id] = { 
         id: socket.id, 
-        x: 1000, 
-        y: 600, 
+        x: 1100, 
+        y: 400, 
         color: colors[Math.floor(Math.random() * colors.length)], 
         name: "Osman" 
     };
+
     io.emit('currentPlayers', players);
-    socket.on('playerMovement', (data) => {
-        if (players[socket.id]) { 
-            players[socket.id].x = data.x; players[socket.id].y = data.y;
-            socket.broadcast.emit('playerMoved', players[socket.id]); 
+
+    socket.on('playerMovement', (movementData) => {
+        if (players[socket.id]) {
+            players[socket.id].x = movementData.x;
+            players[socket.id].y = movementData.y;
+            socket.broadcast.emit('playerMoved', players[socket.id]);
         }
     });
-    socket.on('disconnect', () => { delete players[socket.id]; io.emit('playerDisconnected', socket.id); });
+
+    socket.on('disconnect', () => {
+        delete players[socket.id];
+        io.emit('playerDisconnected', socket.id);
+    });
 });
-http.listen(process.env.PORT || 3000);
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log(`Sunucu ${PORT} portunda aktif.`));
