9ff4d61251c240bc44761625522d75c970cac3f0
OsmanBurda <osmanceviker0@gmail.com>
2026-01-31 21:56:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,87 +1,41 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-// Oyuncu
-const player = {
-  x: 60,
-  y: 60,
-  size: 22,
-  speed: 3
-};
+app.use(express.static("public"));
 
-// Duvarlar (Among Us tarzı odalar)
-const walls = [
-  // Dış sınırlar
-  {x: 0, y: 0, w: 600, h: 20},
-  {x: 0, y: 380, w: 600, h: 20},
-  {x: 0, y: 0, w: 20, h: 400},
-  {x: 580, y: 0, w: 20, h: 400},
+const players = {};
 
-  // Odalar
-  {x: 100, y: 80, w: 300, h: 20},
-  {x: 100, y: 80, w: 20, h: 200},
-  {x: 380, y: 80, w: 20, h: 200},
-  {x: 100, y: 260, w: 300, h: 20},
+io.on("connection", socket => {
+  players[socket.id] = {
+    x: 60,
+    y: 60,
+    color: `hsl(${Math.random()*360},100%,50%)`
+  };
 
-  // Koridor
-  {x: 250, y: 20, w: 20, h: 80}
-];
+  socket.emit("currentPlayers", players);
+  socket.broadcast.emit("newPlayer", {
+    id: socket.id,
+    player: players[socket.id]
+  });
 
-// Klavye
-const keys = {};
-document.addEventListener("keydown", e => keys[e.key] = true);
-document.addEventListener("keyup", e => keys[e.key] = false);
+  socket.on("move", data => {
+    if (players[socket.id]) {
+      players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
+      socket.broadcast.emit("playerMoved", {
+        id: socket.id,
+        x: data.x,
+        y: data.y
+      });
+    }
+  });
 
-// Çarpışma kontrolü
-function isColliding(x, y) {
-  return walls.some(w =>
-    x < w.x + w.w &&
-    x + player.size > w.x &&
-    y < w.y + w.h &&
-    y + player.size > w.y
-  );
-}
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    io.emit("playerDisconnected", socket.id);
+  });
+});
 
-// Güncelleme
-function update() {
-  let nx = player.x;
-  let ny = player.y;
-
-  if (keys["ArrowUp"] || keys["w"]) ny -= player.speed;
-  if (keys["ArrowDown"] || keys["s"]) ny += player.speed;
-  if (keys["ArrowLeft"] || keys["a"]) nx -= player.speed;
-  if (keys["ArrowRight"] || keys["d"]) nx += player.speed;
-
-  if (!isColliding(nx, player.y)) player.x = nx;
-  if (!isColliding(player.x, ny)) player.y = ny;
-}
-
-// Çizim
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Duvarlar
-  ctx.fillStyle = "#555";
-  walls.forEach(w => ctx.fillRect(w.x, w.y, w.w, w.h));
-
-  // Oyuncu (Among Us karakteri gibi)
-  ctx.fillStyle = "red";
-  ctx.beginPath();
-  ctx.arc(
-    player.x + player.size / 2,
-    player.y + player.size / 2,
-    player.size / 2,
-    0,
-    Math.PI * 2
-  );
-  ctx.fill();
-}
-
-// Oyun döngüsü
-function loop() {
-  update();
-  draw();
-  requestAnimationFrame(loop);
-}
-
-loop();
+http.listen(process.env.PORT || 3000);
