4046bd1d65c841fea980b8c413ab4af65bf84809
OsmanBurda <osmanceviker0@gmail.com>
2026-01-31 22:40:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
-app.use(express.static(__dirname + '/'));
-
-let players = {};
-const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'cyan'];
-
-io.on('connection', (socket) => {
-    // 2.5 Kat Ölçekli Güvenli Başlangıç
-    players[socket.id] = {
-        id: socket.id,
-        x: 750, 
-        y: 450, 
-        color: colors[Math.floor(Math.random() * colors.length)],
-        name: "Osman"
-    };
-
-    io.emit('currentPlayers', players);
-
-    socket.on('playerMovement', (movementData) => {
-        if (players[socket.id]) {
-            players[socket.id].x = movementData.x;
-            players[socket.id].y = movementData.y;
-            socket.broadcast.emit('playerMoved', players[socket.id]);
-        }
-    });
-
-    socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('playerDisconnected', socket.id);
-    });
-});
+const http = require("http").createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http);
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Sunucu ${PORT} portunda aktif.`));
+
+// static site
+app.use(express.static("public"));
+
+// oyuncular
+const players = {};
+
+// socket
+io.on("connection", socket => {
+  players[socket.id] = {
+    x: 60,
+    y: 60,
+    color: `hsl(${Math.random() * 360}, 100%, 50%)`
+  };
+
+  socket.emit("init", {
+    id: socket.id,
+    players
+  });
+
+  socket.broadcast.emit("player-join", {
+    id: socket.id,
+    player: players[socket.id]
+  });
+
+  socket.on("move", data => {
+    if (!players[socket.id]) return;
+
+    players[socket.id].x = data.x;
+    players[socket.id].y = data.y;
+
+    socket.broadcast.emit("player-move", {
+      id: socket.id,
+      x: data.x,
+      y: data.y
+    });
+  });
+
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    io.emit("player-leave", socket.id);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
