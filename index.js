45c9ae60f85abf7bc1cd953846f19ba7f4890646
OsmanBurda <osmanceviker0@gmail.com>
2026-01-31 18:55:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,38 +4,50 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// Statik dosyaları (html, css) sunucuya tanıtıyoruz
+// Dosyaların dışarı açılması
 app.use(express.static(__dirname));
 
-// Ana sayfaya girince index.html'i gönder
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+// Ana sayfa yönlendirmesi
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 let players = {};
 
 io.on('connection', (socket) => {
-    console.log('Oyuncu bağlandı:', socket.id);
+    console.log('Yeni oyuncu katıldı: ' + socket.id);
+    
+    // Rastgele başlangıç pozisyonu ve renk
     players[socket.id] = {
-        x: 100, y: 100,
+        x: Math.floor(Math.random() * 300) + 50,
+        y: Math.floor(Math.random() * 300) + 50,
         color: "#" + Math.floor(Math.random()*16777215).toString(16),
         id: socket.id
     };
-    io.emit('currentPlayers', players);
 
-    socket.on('playerMovement', (data) => {
+    // Mevcut oyuncuları yeni gelene gönder
+    socket.emit('currentPlayers', players);
+    // Yeni geleni diğerlerine bildir
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+
+    // Hareket verisini al ve yay
+    socket.on('playerMovement', (movementData) => {
         if (players[socket.id]) {
-            players[socket.id].x = data.x;
-            players[socket.id].y = data.y;
+            players[socket.id].x = movementData.x;
+            players[socket.id].y = movementData.y;
             io.emit('playerMoved', players[socket.id]);
         }
     });
 
+    // Oyuncu çıkınca temizle
     socket.on('disconnect', () => {
+        console.log('Oyuncu ayrıldı: ' + socket.id);
         delete players[socket.id];
         io.emit('playerDisconnected', socket.id);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Sunucu 3000 portunda hazır!'));
+http.listen(PORT, () => {
+    console.log(`Sunucu ${PORT} portunda çalışıyor...`);
+});
