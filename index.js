09e6ae9be61012b9a376288ecb6bb7e9f835e331
OsmanBurda <osmanceviker0@gmail.com>
2026-01-31 22:43:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,49 +6,39 @@ const io = new Server(http);
 
 const PORT = process.env.PORT || 3000;
 
-// static site
-app.use(express.static("public"));
+// root'tan dosya ver
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/client.js", (req, res) => {
+  res.sendFile(__dirname + "/client.js");
+});
 
 // oyuncular
 const players = {};
 
-// socket
 io.on("connection", socket => {
   players[socket.id] = {
     x: 60,
     y: 60,
-    color: `hsl(${Math.random() * 360}, 100%, 50%)`
+    color: `hsl(${Math.random() * 360},100%,50%)`
   };
 
-  socket.emit("init", {
-    id: socket.id,
-    players
-  });
-
-  socket.broadcast.emit("player-join", {
-    id: socket.id,
-    player: players[socket.id]
-  });
+  socket.emit("init", { id: socket.id, players });
+  socket.broadcast.emit("join", { id: socket.id, player: players[socket.id] });
 
   socket.on("move", data => {
     if (!players[socket.id]) return;
-
     players[socket.id].x = data.x;
     players[socket.id].y = data.y;
-
-    socket.broadcast.emit("player-move", {
-      id: socket.id,
-      x: data.x,
-      y: data.y
-    });
+    socket.broadcast.emit("move", { id: socket.id, x: data.x, y: data.y });
   });
 
   socket.on("disconnect", () => {
     delete players[socket.id];
-    io.emit("player-leave", socket.id);
+    io.emit("leave", socket.id);
   });
 });
 
-http.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+http.listen(PORT, () => console.log("Server up:", PORT));
