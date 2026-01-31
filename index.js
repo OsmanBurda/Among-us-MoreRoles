fad70384a4d9271f1cfe5ac7f77b077c92632f2c
OsmanBurda <osmanceviker0@gmail.com>
2026-01-31 23:11:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,30 +8,20 @@ let players = {};
 const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'cyan'];
 
 io.on('connection', (socket) => {
-    // Haritadaki Kafeterya boşluğu (Resim ölçeğine göre)
     players[socket.id] = { 
         id: socket.id, 
-        x: 800, 
-        y: 350, 
+        x: 1000, 
+        y: 600, 
         color: colors[Math.floor(Math.random() * colors.length)], 
         name: "Osman" 
     };
-
     io.emit('currentPlayers', players);
-
     socket.on('playerMovement', (data) => {
         if (players[socket.id]) { 
-            players[socket.id].x = data.x; 
-            players[socket.id].y = data.y;
+            players[socket.id].x = data.x; players[socket.id].y = data.y;
             socket.broadcast.emit('playerMoved', players[socket.id]); 
         }
     });
-
-    socket.on('disconnect', () => { 
-        delete players[socket.id]; 
-        io.emit('playerDisconnected', socket.id); 
-    });
+    socket.on('disconnect', () => { delete players[socket.id]; io.emit('playerDisconnected', socket.id); });
 });
-
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Sunucu ${PORT} portunda aktif.`));
+http.listen(process.env.PORT || 3000);
